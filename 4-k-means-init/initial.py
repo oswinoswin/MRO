3002b3e790684bd61d95ba4bab08ec5a7ccb9d14
oswonoswin <atori@tlen.pl>
2017-11-21 22:50:23 +0100

    dfsgsdfgsd

--- 4-k-means-init/initial.py
+++ 4-k-means-init/initial.py
@@ -49,11 +49,11 @@ def chunk(xs, n):
     random.shuffle(ys)
     ylen = len(ys)
     size = int(ylen / n)
-    chunks = [ys[0+size*i : size*(i+1)] for i in range(n)]
-    leftover = ylen - size*n
-    edge = size*n
+    chunks = [ys[0 + size * i: size * (i + 1)] for i in range(n)]
+    leftover = ylen - size * n
+    edge = size * n
     for i in range(leftover):
-            chunks[i%n].append(ys[edge+i])
+        chunks[i % n].append(ys[edge + i])
     return chunks
 
 
@@ -62,8 +62,6 @@ def random_partition_init(dane, k):
 
     result = []
 
-    print("random partition ")
-    print(tmp)
     for row in tmp:
         x = np.array(row)[:, 0]
         y = np.array(row)[:, 1]
@@ -75,22 +73,39 @@ def random_partition_init(dane, k):
 load_data()
 
 data_np = np.array(data)
+max_it_range = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 50, 100, 200, 300, 400, 600, 800, 1000, 1200]
+n_runs = 5
+inertia_k_means_random = np.zeros((len(max_it_range), n_runs))
+inertia_k_means_plus = np.zeros((len(max_it_range), n_runs))
 
-forgy_init(data_np, 9)
+for j, max_it in enumerate(max_it_range):
+    print(inertia_k_means_random[j])
+    for i in range(n_runs):
+        print("i = {}, j = {}".format(i, j))
+        k_means_random = KMeans(n_clusters=9, random_state=i, init='random', n_init=1, max_iter=max_it).fit(data_np)
+        k_means_plus = KMeans(n_clusters=9, random_state=i, init='k-means++', n_init=1, max_iter=max_it).fit(data_np)
+        inertia_k_means_random[j, i] = k_means_random.inertia_
+        inertia_k_means_plus[j, i] = k_means_plus.inertia_
+    print(inertia_k_means_random[j])
 
-k_means_random = KMeans(n_clusters=9, random_state=1, init='random', n_init=1, max_iter=800).fit(data_np)
-k_means_random_partition = KMeans(n_clusters=9, random_state=8, init=random_partition_init(data_np, 9), n_init=1, max_iter=800).fit(data_np)
+print(inertia_k_means_random)
+print(inertia_k_means_plus)
+
+k_means_random_partition = KMeans(n_clusters=9, random_state=8, init=random_partition_init(data_np, 9), n_init=1,
+                                  max_iter=800).fit(data_np)
 k_means_forgy = KMeans(n_clusters=9, random_state=8, init=forgy_init(data_np, 9), n_init=1, max_iter=800).fit(data_np)
-k_means_plus = KMeans(n_clusters=9, random_state=8, init='k-means++', n_init=1, max_iter=800).fit(data_np)
+k_means_plus = KMeans(n_clusters=9, random_state=8, init='k-means++', n_init=5, max_iter=800).fit(data_np)
 
-
-
+lol = np.array(k_means_plus.cluster_centers_)
+print(lol)
+print(lol.max())
 
 plt.title("k-means")
 plt.xlabel("x")
 plt.ylabel("y")
 plt.scatter(data_np[:, 0], data_np[:, 1], c=k_means_forgy.labels_)
-plt.show()
+#plt.show()
+
 
 '''
 plt.subplot(1, 1, 1, aspect='equal')
